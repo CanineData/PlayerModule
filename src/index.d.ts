6bde009783f67592e1bd14a2df2f79f14682c235
CanineData62816 <spstreet09@gmail.com>
2024-07-19 15:33:53 +0200

    Update package to only export type instance an instance of the class

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,15 +1,12 @@
 import ControlModule, * as ControlTypes from "./ControlModules"
 import CameraModule, * as CameraTypes from "./CameraModules"
 
-declare class PlayerModule {
+export default class PlayerModule {
     private cameras: CameraModule
     private controls: ControlModule
 
     public GetCameras: (this: PlayerModule) => CameraModule
     public GetControls: (this: PlayerModule) => ControlModule
 }
-
-declare const module: PlayerModule
-export default module
 
 export { ControlTypes, CameraTypes }
